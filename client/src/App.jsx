12e3f121763790929d0f600edbc9a745f84052ae
Caleb Morse <calebmorse.code@gmail.com>
2025-11-03 15:56:50 -0600

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Supabase, Express, React Todo List App</h1>
+    </>
+  );
+}
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 import cors from 'cors';
 import express from 'express';
-import secPrograms from './db/secPrograms.js';
 const app = express();
 const PORT = 3000;
 
@@ -8,14 +7,6 @@ app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello, server world!');
-});
-
-app.get('/api/hello', (req, res) => {
-  res.send({ message: 'Hello from the api on my express server!' });
-});
-
-app.get('/api/sec/teams', (req, res) => {
-  res.send(secPrograms);
 });
 
 app.listen(PORT, () => {
